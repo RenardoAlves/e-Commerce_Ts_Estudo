fd3c3285416e64c0ad6e46c0818ad8e8471c8266
Renardo Alves <61791273+RenardoAlves@users.noreply.github.com>
2026-03-25 19:13:45 -0300

    Esqueleto inicial do projeto

---
+++ back-end/produtosTeste.ts
@@ -0,0 +1,12 @@
+const produtos = [
+  { id: 1, nome: "Camisa", descricao: "Camisa preta" },
+  { id: 2, nome: "Calça", descricao: "Calça jeans" },
+  { id: 3, nome: "Tênis", descricao: "Tênis esportivo" },
+  { id: 4, nome: "Jaqueta", descricao: "Jaqueta couro" },
+  { id: 5, nome: "Corrente", descricao: "Corrente aço com pingente de cruz"},
+  { id: 6, nome: "Camisa", descricao: "Camisa preta" },
+  { id: 7, nome: "Calça", descricao: "Calça jeans" },
+  { id: 8, nome: "Tênis", descricao: "Tênis esportivo" }
+];
+
+export default produtos;
