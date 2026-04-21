4be25457babdc29c305434386f5430028fc89568
Renardo Alves <61791273+RenardoAlves@users.noreply.github.com>
2026-04-21 09:55:41 -0300

    API de registro integrada na UI

--- front-end/src/pages/SignUpPage.tsx
+++ front-end/src/pages/SignUpPage.tsx
@@ -2,11 +2,40 @@ import { Button } from "@/components/ui/button";
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const SignUpPage = () => {
 
     const navigate = useNavigate();
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+
+    const handleRegister = async (e: any) => {
+        e.preventDefault()
+
+        setError('')
+
+        try {
+            await axios.post('/api/auth/register', {
+                name,
+                email,
+                password
+            })
+
+            setSuccess('Usuário cadastrado com sucesso')
+            setError('')
+
+        } catch {
+            setError('Erro ao registrar usuário')
+            setSuccess('')
+        }
+    }
 
     return (
         <div className="flex w-[98vw] justify-center p-20">
@@ -20,8 +49,8 @@ const SignUpPage = () => {
                         <Button onClick={() => navigate('/login')} variant="link">Login</Button>
                     </CardAction>
                 </CardHeader>
-                <CardContent>
-                    <form>
+                <form onSubmit={handleRegister}>
+                    <CardContent>
                         <div className="flex flex-col gap-6">
                             <div className="grid gap-2">
                                 <Label htmlFor="name">Name</Label>
@@ -30,6 +59,8 @@ const SignUpPage = () => {
                                     type="name"
                                     placeholder="Your name here"
                                     required
+                                    value={name}
+                                    onChange={(e) => setName(e.target.value)}
                                 />
                             </div>
                             <div className="grid gap-2">
@@ -39,22 +70,34 @@ const SignUpPage = () => {
                                     type="email"
                                     placeholder="email@example.com"
                                     required
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
-                            <div className="grid gap-2">
+                            <div className="grid gap-2 mb-5">
                                 <div className="flex items-center">
                                     <Label htmlFor="password">Password</Label>
                                 </div>
-                                <Input id="password" type="password" required />
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
                             </div>
                         </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex-col gap-2">
-                    <Button type="submit" className="w-full">
-                        Sign Up
-                    </Button>
-                </CardFooter>
+                        <div>
+                            {error && <p className="text-red-500">{error}</p>}
+                            {success && <p className="text-green-500">{success}</p>}
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex-col gap-2">
+                        <Button type="submit" className="w-full">
+                            Sign Up
+                        </Button>
+                    </CardFooter>
+                </form>
             </Card>
         </div>
     )
