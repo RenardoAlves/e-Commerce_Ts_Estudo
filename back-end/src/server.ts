1000a6aba303a216e15567dd2dfde50f6eabf18c
Renardo Alves <61791273+RenardoAlves@users.noreply.github.com>
2026-05-06 14:58:41 -0300

    Configurado CORS para aceitar requisições remotas da Vercel

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,9 +1,17 @@
 import 'dotenv/config';
 import express from "express";
+import cors from 'cors';
 import cartRoutes from "./routes/cart.routes.ts";
 import authRoutes from "./routes/auth.routes.ts";
 
 const app = express();
+
+app.use(cors({
+    origin: 'https://e-commerce-ts-estudo-21p1.vercel.app/',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}))
+
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
